83c813a7a8a54a34cb040190a75e705db0f29046
OscarWoHA <ohalland@gmail.com>
2016-11-06 13:31:50 +0100

    Final and last changes

--- src/main/java/org/devathon/contest2016/ai/AnnaStand.java
+++ src/main/java/org/devathon/contest2016/ai/AnnaStand.java
@@ -42,10 +42,10 @@ public class AnnaStand {
 		this.instance.setCustomNameVisible(true);
 
 		this.instance.setHelmet(this.getFuckingSkullFromMojangAndApplyItToAnItemStack("eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2QzYTdkMTAzYjljNGQ0YWI2ODAzNjlhMTQyYTEwYzllZWE4MDdlOGU3Mjc2ZDhlMmRlNzczM2E2ZDk1ZWY3In0="));
-		
+
 		ItemStack book = new ItemStack(Material.BOOK, 1);
 		this.instance.setItemInHand(book);	
-		
+
 		/*
 		 * All this shitty code for some shitty bot that I wrote just for the lulz <3
 		 * Creds to the French dude on Discord for suggesting that Anna should have a pink chestplate on her ;)
@@ -54,19 +54,19 @@ public class AnnaStand {
 		LeatherArmorMeta meta = (LeatherArmorMeta) chest.getItemMeta();
 		meta.setColor(Color.PURPLE);
 		chest.setItemMeta(meta);
-		
+
 		this.instance.setChestplate(chest);
 	}
-	
+
 	//This code is awesome
 	public void updateFaceDirection(Location pl) {
 		Vector from = new Vector(this.instance.getLocation().getX(), this.instance.getLocation().getY(), this.instance.getLocation().getZ());
 		Vector to  = new Vector(pl.getX(), pl.getY(), pl.getZ());
-		 
+
 		Vector vector = to.subtract(from);
 		Location newloc = this.instance.getLocation();
 		newloc.setDirection(vector);
-		
+
 		this.instance.teleport(newloc);
 	}
 
@@ -74,34 +74,34 @@ public class AnnaStand {
 	public ArmorStand getStand() {
 		return this.instance;
 	}
-	
+
 	//What am I even doing here
 	public void remove() {
 		this.instance.remove();
 	}
-	
+
 	public void talkToPlayer(Player p) {
 		new TalkTask(new String[] {
 				ChatColor.GOLD + "Hello! My name is Anna. I am a computing machine, and definetly your only possible future girlfriend.",
 				ChatColor.GOLD + "You can ask me any question, and I'll try to answer it!", 
 				ChatColor.GRAY + "Type a message into the chat starting with \"!bot\" to ask me stuff.."
-				}, p).startTalking();;
-	
+		}, p).startTalking();;
+
 	}
 
 	//Static because I'm to lazy to infer an Location argument in constructor xD
 	public static AnnaStand createAnnaStand(Location l) {
 		return new AnnaStand((ArmorStand) l.getWorld().spawnEntity(l, EntityType.ARMOR_STAND));
 	}
-	
+
 	//This...this fucking code...mojang and their systems man
 	public ItemStack getFuckingSkullFromMojangAndApplyItToAnItemStack(String skinURL) {
 		ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short) 3);
-		
+
 		if (skinURL.isEmpty()) return head;
-		
+
 		ItemMeta headMeta = head.getItemMeta();
-		
+
 		Field profileField = null;
 		try {
 			profileField = headMeta.getClass().getDeclaredField("profile");
@@ -111,43 +111,50 @@ public class AnnaStand {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		head.setItemMeta(headMeta);
 		return head;
 	}
-	
+
 	//Gameprofiles are neat, I guess.. 
 	public GameProfile getNewProf(String skinURL) {
-	    GameProfile newProf = new GameProfile(UUID.randomUUID(), null);
-	    
-	    newProf.getProperties().put("textures", new Property("textures", Base64Coder.encodeString("{textures:{SKIN:{url:\"" + skinURL + "\"}}}")));
-	    
-	    return newProf;
+		GameProfile newProf = new GameProfile(UUID.randomUUID(), null);
+
+		newProf.getProperties().put("textures", new Property("textures", Base64Coder.encodeString("{textures:{SKIN:{url:\"" + skinURL + "\"}}}")));
+
+		return newProf;
 	}
 
 	public class TalkTask extends BukkitRunnable {
 		private String[] messages;
 		private CommandSender receiver;
-		
+		private boolean running = false;
+
 		TalkTask(String[] messages, CommandSender receiver) {
 			this.messages = messages;
 			this.receiver = receiver;
 		}
-		
+
 		@Override
 		public void run() {
+			running = true;
 			
-			try {
-				for(String msg : this.messages) {
-					this.receiver.sendMessage(msg);
-					Thread.sleep(2000);
+			while(running) {
+				try {
+					for(String msg : this.messages) {
+						this.receiver.sendMessage(msg);
+						Thread.sleep(600);
+					} 
+				} catch (InterruptedException e) {
+					System.out.println("Talk-task was interrupted..");
 				}
-			} catch (InterruptedException e) {
-				System.out.println("Talk-task was interrupted..");
+				
+				running = false;
 			}
-			
+
+			this.cancel();
 		}
-		
+
 		public void startTalking() {
 			this.runTask(DevathonPlugin.getInstance());
 		}
