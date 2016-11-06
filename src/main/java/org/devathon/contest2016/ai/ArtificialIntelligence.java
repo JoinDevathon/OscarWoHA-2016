326bb1f3d887e51e43613174aa826ba099328cc1
OscarWoHA <ohalland@gmail.com>
2016-11-06 04:32:33 +0100

    Fixed some small tiny things

--- src/main/java/org/devathon/contest2016/ai/ArtificialIntelligence.java
+++ src/main/java/org/devathon/contest2016/ai/ArtificialIntelligence.java
@@ -8,14 +8,14 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class ArtificialIntelligence {
 	public HashSet<String> sentences = new HashSet<String>();
-	
+
 	/*
 	 * This function here is the fun part
 	 * I wrote it because I really really wanted to do something with bots and AI-kinda-ish-stuff-I-guess
 	 */
 	public String getMatch(String[] messageMatchables) {
 		ArrayList<String> possibleMatches = new ArrayList<String>();
-		
+
 		for(String matchable : messageMatchables) {
 			Iterator<String> sentencesIterator = this.sentences.iterator();
 			while(sentencesIterator.hasNext()) {
@@ -25,18 +25,18 @@ public class ArtificialIntelligence {
 				}
 			}
 		}
-		
+
 		Pair<String, Integer> leader = null;
-		
+
 		Iterator<String> possibleIterator = possibleMatches.iterator();
 		while(possibleIterator.hasNext()) {
 			String possibleSentence = possibleIterator.next();
-			
+
 			String replacedSentence = possibleSentence.replaceAll("[^\\w\\s]", "");
 			String[] possibles = replacedSentence.split(" ");
-			
+
 			int matchCount = 0;
-			
+
 			for(String possible : possibles) {
 				for(String matchable : messageMatchables) {
 					if(possible.equalsIgnoreCase(matchable)) {
@@ -44,16 +44,20 @@ public class ArtificialIntelligence {
 					}
 				}
 			}
-			
+
 			if(leader == null) { leader = Pair.of(possibleSentence, matchCount); continue; }
-			
+
 			if(leader.getValue() <= matchCount) {
 				leader = Pair.of(possibleSentence, matchCount);
 			}
 		}
-		
+
+        if(leader == null) {
+            return "Grr.. Couldn't find an answer for you baby.";
+        }
+
 		System.out.println("Best match is \"" + leader.getKey() + "\" with " + leader.getValue() + " matches..");
 		return leader.getKey();
 	}
-	
+
 }
